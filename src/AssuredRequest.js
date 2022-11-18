26c44728d8f4204bd4d71881272ba4688adc993c
Gabriel <gabriel@pop-os.localdomain>
2022-11-18 20:51:51 +0000

    Add structured request builder

--- src/AssuredRequest.js
+++ src/AssuredRequest.js
@@ -1,23 +1,31 @@
+import axios from 'axios';
+
 class AssuredRequest {
 
-    constructor(route, requestHeaders, requestBody) {
+    constructor(keyboardAssured, route, requestHeaders, requestBody) {
+        this.keyboardAssured = keyboardAssured;
         this.route = route;
         this.requestHeaders = requestHeaders;
         this.requestBody = requestBody;
     }
 
-    constructor(route) {
+    constructor(keyboardAssured, route) {
+        this.keyboardAssured = keyboardAssured;
         this.route = route;
     }
 
     setHeaders(requestHeaders) {
         this.requestHeaders = requestHeaders;
+
+        return this;
     }
 
     getHeaders() { return this.requestHeaders; }
 
     setBody(requestBody) {
         this.requestBody = requestBody; 
+
+        return this;
     }
 
     getBody() { 
@@ -26,6 +34,74 @@ class AssuredRequest {
 
     setRoute(route) {
         this.route = route;
+
+        return this;
+    }
+
+    getRoute() { return this.route; }
+
+    getURL() { return this.keyboardAssured.getBaseURL() + this.route; }
+
+    async post() {
+        this.pendingResponse = axios.post(this.getURL(), this.getBody(), this.getHeaders());
+
+        if(this.keyboardAssured.getShouldAwaitResponse()) await this.pendingResponse;
+        return this;
+    }
+
+    async get() {
+        this.pendingResponse = axios.get(this.getURL(), this.getHeaders());
+
+        if(this.keyboardAssured.getShouldAwaitResponse()) await this.pendingResponse;
+        return this;
+    } 
+
+    async delete(){
+        this.pendingResponse = axios.delete(this.getURL(), this.getHeaders());
+
+        if(this.keyboardAssured.getShouldAwaitResponse()) await this.pendingResponse;
+        return this;
+    }
+
+    async put() {
+        this.pendingResponse = axios.put(this.getURL(), this.getBody(), this.getHeaders());
+
+        if(this.keyboardAssured.getShouldAwaitResponse()) await this.pendingResponse;
+        return this;
+    }
+
+    async patch() {
+        this.pendingResponse = axios.patch(this.getURL(), this.getBody(), this.getHeaders());
+
+        if(this.keyboardAssured.getShouldAwaitResponse()) await this.pendingResponse;
+        return this;
+    }
+
+    getResponse() { return this.pendingResponse; }
+
+    async getResponseData() { 
+        if(this.pendingResponse === undefined) return;
+        if(this.pendingResponse?.isFullfilled()) return this.pendingResponse.data;
+        
+        return await this.pendingResponse.then((response) => {
+            return response.data;
+        })
+    }
+
+    async getStatusCode(){
+        if(this.pendingResponse === undefined) return;
+
+        const response = await this.getResponse();
+
+        return response.status;
+    }
+
+    async getResponseHeaders() {
+        if(this.pendingResponse === undefined) return;
+
+        const response = await this.getResponse();
+
+        return response.headers;
     }
     
 }
--- src/KeyboardAssured.js
+++ src/KeyboardAssured.js
@@ -1,18 +1,31 @@
-import chai from 'chai';
-import axios from 'axios';
 import AssuredRequest from './AssuredRequest';
 
 class KeyboardAssured {
 
     constructor(baseURL) {
-        if(baseURL !== undefined) this.baseURL = baseURL;
+        this.baseURL = baseURL;
+        global.baseURL = baseURL;
     }
 
     setBaseURL(baseURL) {
         this.baseURL = baseURL;
     }
 
+    setShouldAwaitResponse(shouldAwait) {
+        this.shouldAwait = shouldAwait;
+    }
+
+    getShouldAwaitResponse() { return this.shouldAwait; }
+
     getBaseURL() { return this.baseURL; }
+
+    createRequest(route) {
+        return new AssuredRequest(this, route);
+    }
+
+    createRequest(route, requestHeaders, requestBody) {
+        return new AssuredRequest(this, route, requestHeaders, requestBody);
+    }
 }
 
 export default KeyboardAssured;
