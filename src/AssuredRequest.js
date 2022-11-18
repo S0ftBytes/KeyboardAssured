8f3d4d42496170fd660d2a19e0da0d22e698da04
S0ftBytes <gkane2005@gmail.com>
2022-11-18 21:29:45 +0000

    Add assertion methods to AssuredRequest

--- src/AssuredRequest.js
+++ src/AssuredRequest.js
@@ -103,6 +103,40 @@ class AssuredRequest {
 
         return response.headers;
     }
+
+    async expectCode(expectedCode) {
+        const code = await this.getStatusCode();
+
+        if(code !== expectedCode) throw new Error(`Expected status code ${expectedCode}, but got ${code}`);
+
+        return this;
+    }
+
+    async expectHeader(header, expectedValue) {
+        const headers = await this.getResponseHeaders();
+
+        if(headers[header] !== expectedValue) throw new Error(`Expected header ${header} to be ${expectedValue}, but got ${headers[header]}`);
+
+        return this;
+    }
+
+    async expectValue(expectedProp, expectedValue) {
+        const body = await this.getResponseData();
+
+        const assertedProp = body[expectedProp];
+        if(assertedProp !== expectedValue) throw new Error(`Expected ${expectedProp} to have the value ${expectedValue}, but got ${assertedProp}`);
+
+        return this;
+    }
+
+    async expectToIncludeProp(expectedProp) {
+        const body = await this.getResponseData();
+
+        if(body[expectedProp] === undefined) throw new Error(`Expected ${expectedProp} to be included in the response body, but it was not`);
+
+        return this;
+    }
+
     
 }
 
