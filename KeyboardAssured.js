c5c2bdf56d08ef35284c9bb1917afbcc9427b0db
Gabriel <gabriel@pop-os.localdomain>
2022-11-18 20:05:21 +0000

    Create initial package class structure

--- KeyboardAssured.js
+++ KeyboardAssured.js
@@ -1,5 +1,17 @@
+import chai from 'chai';
+import axios from 'axios';
+
 class KeyboardAssured {
-    
+
+    constructor(baseURL) {
+        if(baseURL !== undefined) this.baseURL = baseURL;
+    }
+
+    setBaseURL(baseURL) {
+        this.baseURL = baseURL;
+    }
+
+    getBaseURL() { return this.baseURL; }
 }
 
 export default KeyboardAssured;
